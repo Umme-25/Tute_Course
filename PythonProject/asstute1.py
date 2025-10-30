7c7d3d0671684ea6f597f113b94ef93c0cab65e7
Umme-25 <ummehabiba2241@gmail.com>
2025-10-30 22:42:52 +0530

    initial commit

---
+++ PythonProject/asstute1.py
@@ -0,0 +1,21 @@
+    #ASSIGNMENT 1
+#Task 1
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+#operations
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+#displaying the results.
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",mul)
+print("Division:",div)
+
+#Task 2
+first_name=input("Enter your first name:")
+last_name=input("Enter your last name:")
+full_name=first_name+" "+last_name
+print(f"Hello, {full_name}! Welcome to the Python program.")
+
